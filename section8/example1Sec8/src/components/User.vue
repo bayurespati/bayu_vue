fc0ac7c8e17587f9fa229cb6e823fd6df375f04d
bayurespati <respatibayu48@gmail.com>
2017-01-12 19:25:07 +0700

    communication between Sibling  components

--- section8/example1Sec8/src/components/User.vue
+++ section8/example1Sec8/src/components/User.vue
@@ -9,10 +9,13 @@
                 <app-user-detail
                         :myName="name"
                         @nameWasReset="name = $event"
-                        :resetFn="resetName"></app-user-detail>
+                        :resetFn="resetName"
+                        :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit
+                        :userAge="age"
+                        @ageWasEdited="age = $event"></app-user-edit>
             </div>
         </div>
     </div>
@@ -25,7 +28,8 @@
     export default {
         data: function(){
             return{
-                name : 'Kaliang'
+                name : 'Kaliang',
+                age : 27
             };
         },
         methods:{
