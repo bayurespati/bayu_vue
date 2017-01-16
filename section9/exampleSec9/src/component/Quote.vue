f6676556e9386de6704f0f669c4a4f8426b35eb3
bayurespati <respatibayu48@gmail.com>
2017-01-16 11:43:58 +0700

    passing content with slot

---
+++ section9/exampleSec9/src/component/Quote.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div{
+        border: 1px Solid #ddd;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px atuo;
+        text-align: center;
+    }
+</style>
