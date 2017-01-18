31514a0e04a4ffbc133d20ce541cee6d17cf295b
bayurespati <respatibayu48@gmail.com>
2017-01-18 15:29:02 +0700

    intial structure for application

--- section10/exampleSec10/src/App.vue
+++ section10/exampleSec10/src/App.vue
@@ -6,7 +6,14 @@
 
 <script>
     export default {
-
+        data: function(){
+          return{
+            quotes:[
+              'just a Quote to see something'
+            ],
+            maxQuotes: 10
+          }
+        }
     }
 </script>
 
