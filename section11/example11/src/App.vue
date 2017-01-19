7168970ad46ece97482cf94ae7d1e73aa05039d5
bayurespati <respatibayu48@gmail.com>
2017-01-19 13:33:08 +0700

    submitting a form

--- section11/example11/src/App.vue
+++ section11/example11/src/App.vue
@@ -103,7 +103,8 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <button
-                  class="btn btn-primary">Submit!
+                  class="btn btn-primary"
+                  @click.prevent="submitted">Submit!
           </button>
         </div>
       </div>
@@ -150,8 +151,14 @@
           gender: '',
           selectedPrioriti: 'High',
           priorities: ['High','Medium','Low'],
-          dataSwitch: true
+          dataSwitch: true,
+          isSubmitted: false
         };
+      },
+      methods:{
+        submitted(){
+          this.isSubmitted = true;
+        }
       },
       components:{
         appSwitch: Switch
