372219085f9ba02b39bd9603a0198ec581584fae
bayurespati <respatibayu48@gmail.com>
2017-05-15 10:38:24 +0700

    animation app and refine code game_project

--- section14/createApp/src/App.vue
+++ section14/createApp/src/App.vue
@@ -1,53 +1,80 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1 class="text-center">The Super Quiz</h1>
             </div>
         </div>
         <hr>
         <div class="row">
             <div class="col-xs12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <component></component>
+                <transition name="flip" mode="out-in">
+                <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
+    export default{
+        data(){
+            return{
+                mode: 'app-question'
+            }
+        },
+        methods: {
+            answered(isCorrect){
+                if(isCorrect){
+                    this.mode = 'app-answer'
+                }else{
+                    this.mode = 'app-question';
+                    alert('Wrong, try again!');
+                }
+            }
+        },
+        components:{
+            appQuestion : Question,
+            appAnswer : Answer
+        }
     }
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .flip-enter{
 
-h1, h2 {
-  font-weight: normal;
-}
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    .flip-enter-active{
+        animation: flip-in 0.5s ease-out forwards;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    .flip-leave{
 
-a {
-  color: #42b983;
-}
+    }
+
+    .flip-leave-active{
+        animation: flip-out 0.5s ease-out forwards;
+    }
+
+    @keyframes flip-out{
+        from {
+            transform: rotateY(0deg);
+        }
+        to{
+            transform: rotateY(90deg);
+        }
+    }
+
+    @keyframes flip-in{
+        from{
+            transform: rotateY(90deg);
+        }
+        to{
+            transform: rotateY(0deg);
+        }
+    }
+
 </style>
