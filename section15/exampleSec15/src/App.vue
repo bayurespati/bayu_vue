42f210e8c79d52cb819a28c87f0b522242d1e0c2
bayurespati <respatibayu48@gmail.com>
2017-05-15 12:25:33 +0700

    GETting and transforming data(sending a GET request)

--- section15/exampleSec15/src/App.vue
+++ section15/exampleSec15/src/App.vue
@@ -12,6 +12,14 @@
                     <input class="form-control" type="text" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
+
+                <hr>
+
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -24,7 +32,8 @@
                 user:{
                     username: '',
                     email: ''
-                }
+                },
+                users:[]
             };
         },
         methods:{
@@ -34,6 +43,19 @@
                         console.log(response);
                     }, error =>{
                         console.log(error);
+                    });
+            },
+            fetchData(){
+                this.$http.get('https://vuejs-http-2c8fa.firebaseio.com/data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for(let key in data){
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
                     });
             }
         }
