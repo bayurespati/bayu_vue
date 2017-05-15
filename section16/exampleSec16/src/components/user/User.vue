070ab97bc1f0a324fae731a165fe5f3dc78ca5d1
bayurespati <respatibayu48@gmail.com>
2017-05-15 17:25:50 +0700

    styling active link and navigating from code

--- section16/exampleSec16/src/components/user/User.vue
+++ section16/exampleSec16/src/components/user/User.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>The User page</h1>
+    <div>
+        <h1>The User page</h1>
+        <hr>
+        <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+    </div>
 </template>
+
+<script>
+    export default{
+        methods:{
+            navigateToHome(){
+                this.$router.push('/');
+            }
+        }
+    }
+</script>
