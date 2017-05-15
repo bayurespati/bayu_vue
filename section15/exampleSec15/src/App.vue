199f7bc31748cbbf3785ad4ef4730e32985cde92
bayurespati <respatibayu48@gmail.com>
2017-05-15 11:59:51 +0700

    Posting data to a server (sending a POST request)

--- section15/exampleSec15/src/App.vue
+++ section15/exampleSec15/src/App.vue
@@ -29,7 +29,12 @@
         },
         methods:{
             submit(){
-                console.log(this.user);
+                this.$http.post('https://vuejs-http-2c8fa.firebaseio.com/data.json', this.user)
+                    .then(response => {
+                        console.log(response);
+                    }, error =>{
+                        console.log(error);
+                    });
             }
         }
     }
