9bf00258fccf7e7374478efe7f4b25764a9e8c7a
bayurespati <respatibayu48@gmail.com>
2017-05-15 13:24:32 +0700

    intercepting request and response

--- section15/exampleSec15/src/main.js
+++ section15/exampleSec15/src/main.js
@@ -5,6 +5,15 @@ import App from './App.vue'
 Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vuejs-http-2c8fa.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+    console.log(request);
+    if(request.method == 'POST'){
+        request.method = 'PUT';
+    }
+    next(response => {
+        response.json = () => {return {messages: response.body}}
+    });
+});
 
 new Vue({
   el: '#app',
