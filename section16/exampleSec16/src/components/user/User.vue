0a7bac62fd416fa01f7f748e25bbd79c7fa6c7a5
bayurespati <respatibayu48@gmail.com>
2017-05-16 12:02:47 +0700

    animating route transition

--- section16/exampleSec16/src/components/user/User.vue
+++ section16/exampleSec16/src/components/user/User.vue
@@ -4,7 +4,9 @@
         <hr>
         <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
         <hr>
+        <transition name="slide" mode="out-in">
         <router-view></router-view>
+        </transition>
     </div>
 </template>
 
@@ -17,3 +19,40 @@
         }
     }
 </script>
+
+<style>
+
+    .slide-leave-active{
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave{
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active{
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0%{
+            transform: translateY(0);
+        }
+        100%{
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in{
+        0%{
+            transform: translateY(-30px);
+        }
+        100%{
+            transform: translateY(-0);
+        }
+    }
+
+</style>
--- section16/exampleSec16/src/routes.js
+++ section16/exampleSec16/src/routes.js
@@ -6,18 +6,21 @@ import Home from './components/Home.vue';
 import Header from './components/Header.vue';
 
 export const routes = [
+
     { path: '', name: 'home', components:{
         default: Home,
         'header-top': Header
     } },
+
     { path: '/user', components:{
         default: User,
-        'header-bottom': Header
+        'header-top': Header
     }, children:[
         { path: '', component: UserStart },
         { path: ':id', component: UserDetail },
         { path: ':id/edit', component: UserEdit, name: 'userEdit' }
     ]},
+
     { path: '/redirect-me', redirect: { name:'home' } },
     { path: '*', redirect: '/' }
 ];
