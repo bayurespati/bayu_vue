d55c0444ad2a462a07423f2af04f7716dfc9b054
bayurespati <respatibayu48@gmail.com>
2017-05-16 12:13:48 +0700

    refine animating route transiton

--- section16/exampleSec16/src/components/user/User.vue
+++ section16/exampleSec16/src/components/user/User.vue
@@ -4,7 +4,8 @@
         <hr>
         <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
         <hr>
-        <transition name="slide" mode="out-in">
+        <transition
+                    enter-active-class="animated flipInX">
         <router-view></router-view>
         </transition>
     </div>
@@ -19,40 +20,3 @@
         }
     }
 </script>
-
-<style>
-
-    .slide-leave-active{
-        transition: opacity 1s ease;
-        opacity: 0;
-        animation: slide-out 1s ease-out forwards;
-    }
-
-    .slide-leave{
-        opacity: 1;
-        transform: translateX(0);
-    }
-
-    .slide-enter-active{
-        animation: slide-in 1s ease-out forwards;
-    }
-
-    @keyframes slide-out {
-        0%{
-            transform: translateY(0);
-        }
-        100%{
-            transform: translateY(-30px);
-        }
-    }
-
-    @keyframes slide-in{
-        0%{
-            transform: translateY(-30px);
-        }
-        100%{
-            transform: translateY(-0);
-        }
-    }
-
-</style>
