b4c40ec9c91ab0aeb7cdb6d4efe380e83047dd78
bayurespati <respatibayu48@gmail.com>
2017-05-16 10:45:45 +0700

    naming route

--- section16/exampleSec16/src/components/user/UserDetail.vue
+++ section16/exampleSec16/src/components/user/UserDetail.vue
@@ -4,7 +4,7 @@
         <p>User loaded has ID : {{ $route.params.id }}</p>
         <router-link
                 tag="button"
-                :to="'/user/'+$route.params.id + '/edit'"
+                :to="{ name: 'userEdit' }"
                 class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
