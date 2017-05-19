ad3a55e9e051b488f05bbbaccb5e8de9e47f9e16
bayurespati <respatibayu48@gmail.com>
2017-05-19 16:00:23 +0700

    creating stock component and adding button buy

--- section18/project18/src/components/stocks/Stocks.vue
+++ section18/project18/src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>stocks</h1>
+    <div>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue';
+
+    export default{
+        data(){
+            return {
+                stocks : [
+                    {id : 1, name : 'BMW', price : 100},
+                    {id : 2, name : 'Google', price : 200},
+                    {id : 3, name : 'Apple', price : 250},
+                    {id : 4, name : 'Twiiter', price : 8}
+                ]
+            }
+        },
+        components:{
+          appStock : Stock
+        }
+    }
+</script>
