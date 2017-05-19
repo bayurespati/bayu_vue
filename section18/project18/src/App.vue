6d3febe54992c9a2673b5fd2753fa0de85fc97a5
bayurespati <respatibayu48@gmail.com>
2017-05-19 12:48:40 +0700

    setup project

---
+++ section18/project18/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12">
+                <h1>Set UP</h1>
+            </div>
+        </div>
+    </div>
+</template>
